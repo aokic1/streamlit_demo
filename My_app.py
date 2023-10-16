ffcf71b7d9c0cdd7c6db99470a6e76a8c00d3f99
aokic <armin.okic@endava.com>
2023-10-16 10:09:01 +0200

    test

--- My_app.py
+++ My_app.py
@@ -2,27 +2,27 @@ import streamlit as st
 
 st.write("This our first web app!")
 
-# from sklearn import datasets
+from sklearn import datasets
 
-# data = datasets.load_iris()
+data = datasets.load_iris()
 
 tab1, tab2 = st.tabs(["Data", "Graphs"])
-st.write("@@@@")
+# st.write("@@@@")
 # # st.write(data)
-# import pandas as pd
-# # df = pd.DataFrame(data=data.data, columns=data.feature_names)
+import pandas as pd
+df = pd.DataFrame(data=data.data, columns=data.feature_names)
 
 
-# with tab1:
+with tab1:
 
-#     df
+    df
 
 
-# with tab2:
-#     # import plotly_express as px
-#     x = st.selectbox("Insert X axis:", df.columns.values)
-#     y = st.selectbox("Insert Y axis:", df.columns.values)
+with tab2:
+    import plotly_express as px
+    x = st.selectbox("Insert X axis:", df.columns.values)
+    y = st.selectbox("Insert Y axis:", df.columns.values)
 
-#     # fig = px.scatter(data_frame=df, x=x, y=y)
+    fig = px.scatter(data_frame=df, x=x, y=y)
 
-#     # st.plotly_chart(fig)
+    st.plotly_chart(fig)
